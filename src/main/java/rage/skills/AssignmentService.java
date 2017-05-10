ec15d3a62e230fc80bf46c5a80cb760455197095
avihavai <avihavai@hp8x-64.cs.helsinki.fi>
2017-05-10 15:04:04 +0300

    content

---
+++ src/main/java/rage/skills/AssignmentService.java
@@ -0,0 +1,33 @@
+package rage.skills;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import javax.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AssignmentService {
+
+    private List<String> assignments;
+
+    @PostConstruct
+    public void init() {
+        this.assignments = new ArrayList<>();
+        this.assignments.add("/path/to/exercise1.zip");
+        this.assignments.add("/path/to/exercise2.zip");
+        this.assignments.add("/path/to/exercise3.zip");
+        this.assignments.add("/path/to/exercise4.zip");
+        this.assignments.add("/path/to/exercise5.zip");
+        this.assignments.add("/path/to/exercise6.zip");
+        this.assignments.add("/path/to/exercise7.zip");
+        this.assignments.add("/path/to/exercise8.zip");
+        this.assignments.add("/path/to/exercise9.zip");
+        this.assignments.add("/path/to/exercise10.zip");
+    }
+
+    public String getRandomAssignment(String user) {
+        return this.assignments.get(new Random().nextInt(this.assignments.size()));
+    }
+
+}
---
+++ src/main/java/rage/skills/SkillController.java
@@ -0,0 +1,42 @@
+package rage.skills;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SkillController {
+
+    Map<String, Integer> responseCounts = new HashMap<>();
+
+    @Autowired
+    AssignmentService assignmentService;
+
+    @RequestMapping("/next.json")
+    public Map getNextAssignment(@RequestParam String username) {
+        Map assignmentContent = new TreeMap();
+
+        if (responseCounts.getOrDefault(username, 0) >= 3) {
+            assignmentContent.put("available", false);
+        } else {
+            assignmentContent.put("available", true);
+            assignmentContent.put("zip_url", assignmentService.getRandomAssignment(username));
+        }
+
+        responseCounts.put(username, responseCounts.getOrDefault(username, 0) + 1);
+        return assignmentContent;
+    }
+    
+    @RequestMapping(value = "/*zip", produces = "application/zip")
+    public void downloadContent(HttpServletResponse response) throws Throwable {
+        response.setHeader("Content-Disposition", "attachment; filename=file.zip");
+        Files.copy(Paths.get("src/main/resources/01.AdaLovelace.zip"), response.getOutputStream());
+    }
+}
