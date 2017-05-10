77d5077f88457df3df0dea5d3379a3c76fc8e5ab
Henrik Nygren <nygrenh@gmail.com>
2017-05-10 16:09:30 +0300

    Build configs, checkstyle

--- src/main/java/rage/skills/SkillController.java
+++ src/main/java/rage/skills/SkillController.java
@@ -1,23 +1,23 @@
 package rage.skills;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
-import javax.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SkillController {
 
     Map<String, Integer> responseCounts = new HashMap<>();
 
-    @Autowired
-    AssignmentService assignmentService;
+    @Autowired AssignmentService assignmentService;
 
     @RequestMapping("/next.json")
     public Map getNextAssignment(@RequestParam String username) {
@@ -33,7 +33,7 @@ public class SkillController {
         responseCounts.put(username, responseCounts.getOrDefault(username, 0) + 1);
         return assignmentContent;
     }
-    
+
     @RequestMapping(value = "/*zip", produces = "application/zip")
     public void downloadContent(HttpServletResponse response) throws Throwable {
         response.setHeader("Content-Disposition", "attachment; filename=file.zip");
--- src/main/java/rage/skills/SkillsApplication.java
+++ src/main/java/rage/skills/SkillsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SkillsApplication {
 
     public static void main(String[] args) {
+        System.getProperties().put( "server.port", 3200 );
         SpringApplication.run(SkillsApplication.class, args);
     }
 }
